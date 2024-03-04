47ff7c4422168b074e7d688e27daba0586474b56
srirama2004 <161791032+srirama2004@users.noreply.github.com>
2024-03-04 21:52:22 +0530

    RoomBook

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from "./pages/Login";
-import NoPage from "./pages/Nopage";
-import Signup from './pages/Signup';
-import Payements from './pages/Payment';
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Login />} />
-        <Route path="*" element={<NoPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/payments" element={<Payements />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
