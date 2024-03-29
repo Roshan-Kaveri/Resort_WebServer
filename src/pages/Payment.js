1f694d894c01e8edda3bf62642d485158d5b5a87
Roshan-Kaveri <161740353+Roshan-Kaveri@users.noreply.github.com>
2024-03-29 17:59:05 +0530

    success

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import image from '../components/login_page/asssets/bg.png';
+import { useState } from 'react';
+import Paysucc from './Paysucc';
+
+
 function Payments() {
+    const[openModal, setOpenModal] = useState(false);
     return <div className="p_c">
         <img src={image} alt="" />
         <div className="p_rectangle1">
@@ -15,7 +20,9 @@ function Payments() {
             <div className='p_pass1'>
         <b>CVV:</b> <input type="password" id="p_password1" name="password1" pattern="[0-9]{3}" title="Please enter a valid CVV (3 digits)"></input>
                 <b>EXPIRY:</b> <input type="text" name="text" id="p_text" required></input>
-            </div> <input className = "p_submit" type="submit" value="PAY NOW"></input></div>
+            </div> <input className = "p_submit" type="submit" value="PAY NOW" onClick={() => {setOpenModal(true);}}></input></div>
+            {openModal && <Paysucc closeModal={setOpenModal}/>}
+            
     </div>
 }
 export default Payments;
