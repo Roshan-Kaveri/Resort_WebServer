0035768eb10602c2e45b2a60f62df363bd14ae18
Roshan-Kaveri <roshankaveri93@gmail.com>
2024-12-25 00:31:35 +0530

    Fixed DB Connection

--- Backend/db.js
+++ Backend/db.js
@@ -1,18 +1,13 @@
+require("dotenv").config(); // Load environment variables from .env file
+
 const { Pool } = require("pg");
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "123456",
-//   host: "localhost",
-//   port: 5432, // default Postgres port
-//   database: "postgres",
-// });
 
 const pool = new Pool({
-  user: "ascscs",
-  password: "@7sdDgVUuhCXjD6",
-  host: "postgresql-ascscs.alwaysdata.net",
-  port: 5432, // default Postgres port
-  database: "ascscs_resortdb12",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = {
